017100265ed5291c92bb5cfc57bab356271abf47
sayedbs23 <sayeed@bs-23.net>
2020-09-08 23:00:16 +0600

    Regristation and login primary done

--- Architecture.Web/ClientApp/src/app/Outside-Layout/landing-page/landing-page.component.ts
+++ Architecture.Web/ClientApp/src/app/Outside-Layout/landing-page/landing-page.component.ts
@@ -9,9 +9,22 @@ import { animationRightToggle } from 'src/app/Shared/Modules/animations/animatio
 })
 export class LandingPageComponent implements OnInit {
 
+  menuState: string = 'out';
+  regOpen: string = 'out';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleClick() {
+    this.regOpen ='out'
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
+
+  toggleRegClick() {
+    this.menuState = 'out';
+    this.regOpen = this.regOpen === 'out' ? 'in' : 'out';
   }
 
 }
--- Architecture.Web/ClientApp/src/app/Shared/Modules/animations/animationRightToggle.ts
+++ Architecture.Web/ClientApp/src/app/Shared/Modules/animations/animationRightToggle.ts
@@ -4,10 +4,14 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export const animationRightToggle =
     trigger('animationRightToggle', [
         state('in', style({
-            transform: 'translate3d(0,0,0)'
+            transform: 'translate3d(0,0,0)',
+            opacity: '1',
+            visibility: 'visiable'
         })),
         state('out', style({
-            transform: 'translate3d(100%, 0, 0)'
+            transform: 'translate3d(100%, 0, 0)',
+            opacity: '0',
+            visibility: 'hidden'
         })),
         transition('in => out', animate('400ms ease-in-out')),
         transition('out => in', animate('400ms ease-in-out'))
