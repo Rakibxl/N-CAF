539a9331bb8877f628c9806bab2c2d793a8a966b
Palash <palash924332931@gmail.com>
2020-10-02 23:11:54 +0600

    Local updated

--- Architecture.Web/ClientApp/src/app/Outside-Layout/landing-page/landing-page.component.ts
+++ Architecture.Web/ClientApp/src/app/Outside-Layout/landing-page/landing-page.component.ts
@@ -2,34 +2,60 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { animationRightToggle } from 'src/app/Shared/Modules/animations/animationRightToggle';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  animations: [animationRightToggle]
+    selector: 'app-landing-page',
+    templateUrl: './landing-page.component.html',
+    animations: [animationRightToggle]
 })
 export class LandingPageComponent implements OnInit {
 
-  menuState: string = 'out';
-  regOpen: string = 'out';
+    public openLogin: string = 'out';
+    public regOpen: string = 'out';
+    public openResetPass: string = "out";
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  toggleClick() {
-    this.regOpen ='out'
-    this.menuState = this.menuState === 'out' ? 'in' : 'out';
-  }
+    public showLoginForm() {
+        this.regOpen = 'out';
+        this.openResetPass = "out";
+        this.openLogin = this.openLogin === 'out' ? 'in' : 'out';
+    }
 
-  toggleRegClick() {
-    this.menuState = 'out';
-    this.regOpen = this.regOpen === 'out' ? 'in' : 'out';
-  }
+    public showRegForm() {
+        this.openLogin = 'out';
+        this.openResetPass = "out";
+        this.regOpen = this.regOpen === 'out' ? 'in' : 'out';
+    }
 
-  closePanel() {
-    this.menuState = 'out';
-    this.regOpen = 'out';
-  }
-  
+    closePanel() {
+        this.openResetPass = "out";
+        this.openLogin = 'out';
+        this.regOpen = 'out';
+    }
+    public showingPasswordResetForm() {
+        this.openLogin = 'out';
+        this.regOpen = "out";
+        this.openResetPass = this.openResetPass === 'out' ? 'in' : 'out';
+    }
+
+    public fnOnClickLoginComponent(event: any) {
+        console.log("event on click: ", event);
+        if (event == "forgotPassword") {
+            this.showingPasswordResetForm();
+        } else if (event == "registration") {
+            this.showRegForm();
+        }
+
+    }
+    public fnOnClickRegComponent(event: any) {
+        console.log("event on click: ", event);
+        if (event == "loginForm") {
+            this.showLoginForm();
+        }
+
+    }
+
 
 }
--- Architecture.Web/ClientApp/src/app/Outside-Layout/user-registration/user-registration.component.ts
+++ Architecture.Web/ClientApp/src/app/Outside-Layout/user-registration/user-registration.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
 })
 export class UserRegistrationComponent implements OnInit {
-
+    @Output() OnClickChange = new EventEmitter() || null;
   constructor() { }
 
   ngOnInit() {
-  }
-
+    }
+    public showingLoginForm() {
+        this.OnClickChange.emit("loginForm");
+    }
 }
