7151dab1458aee9e2edae155e1e2cd6c582c007c
Rakibxl <Administrator@DESKTOP-H7T44MN>
2020-10-07 23:56:46 +0200

    Updated list of missing client profiles

--- Architecture.Web/ClientApp/src/app/Layout/ClientProfile/address-information/address-information.component.ts
+++ Architecture.Web/ClientApp/src/app/Layout/ClientProfile/address-information/address-information.component.ts
@@ -24,11 +24,11 @@ export class AddressInformationComponent implements OnInit {
         let id = 0;
         if (event.action == "new-record") {
             debugger;
-            this.router.navigate(['/client-profile/address-new']);
+            this.router.navigate(['/client-profile/address-info/0']);
             debugger
         }
         else if (event.action == "edit-item") {
-            this.router.navigate(['/client-profile/address-new']);
+            this.router.navigate(['/client-profile/address-info/0']);
         }
     }
 
