b5ec5bffcc071276af222344fd3af7befeef1e59
Palash <palash924332931@gmail.com>
2020-10-08 22:54:49 +0600

    Created Family Information

---
+++ Architecture.Core/Entities/LU/RecordStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Architecture.Core.Entities.LU
+{
+    public class RecordStatus
+    {
+        public int RecordStatusId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
