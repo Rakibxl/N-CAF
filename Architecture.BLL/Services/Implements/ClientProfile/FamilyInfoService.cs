bd9022219a631950736c9fdebb18da7c7a08cdb1
Palash <palash924332931@gmail.com>
2020-11-16 02:32:21 +0600

    Updated Family information

--- Architecture.BLL/Services/Implements/ClientProfile/FamilyInfoService.cs
+++ Architecture.BLL/Services/Implements/ClientProfile/FamilyInfoService.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Architecture.BLL.Services.Interfaces.ClientProfile;
 using Architecture.Core.Entities;
+using Architecture.Core.Repository.Context;
 using Architecture.Core.Repository.Core;
 
 namespace Architecture.BLL.Services.Implements.ClientProfile
 {
-    public class FamilyInfoService : IFamilyInfoService
+    public class FamilyInfoService :Repository<ProfFamilyInfo>, IFamilyInfoService
     {
-        public IRepository<ProfFamilyInfo> familyInfoRepo;
+       
+        public FamilyInfoService(ApplicationDbContext context) : base(context)
+        {
+        }
+
         public async Task<ProfFamilyInfo> AddOrUpdate(ProfFamilyInfo familyInfo)
         {
             try
@@ -17,11 +22,11 @@ namespace Architecture.BLL.Services.Implements.ClientProfile
                 ProfFamilyInfo result;
                 if (familyInfo.FamilyInfoId > 0)
                 {
-                    result = await familyInfoRepo.UpdateAsync(familyInfo);
+                    result = await UpdateAsync(familyInfo);
                 }
                 else
                 {
-                    result = await familyInfoRepo.AddAsync(familyInfo);
+                    result = await AddAsync(familyInfo);
                 }
 
                 return result;
@@ -36,16 +41,16 @@ namespace Architecture.BLL.Services.Implements.ClientProfile
 
         public async Task<int> Delete(int familyInfoId)
         {
-            var result = await familyInfoRepo.DeleteAsync(x=>x.FamilyInfoId== familyInfoId);
+            var result = await DeleteAsync(x=>x.FamilyInfoId== familyInfoId);
             return result;
         }
 
         public async Task<ProfFamilyInfo> GetById(int familyInfoId)
         {
-            var checkVal =await familyInfoRepo.IsExistsAsync(x=>x.FamilyInfoId== familyInfoId);
+            var checkVal =await IsExistsAsync(x=>x.FamilyInfoId== familyInfoId);
             if (checkVal)
             {
-                ProfFamilyInfo result = await familyInfoRepo.GetByIdAsync(familyInfoId);
+                ProfFamilyInfo result = await GetByIdAsync(familyInfoId);
                 return result;
             }
             else
@@ -58,7 +63,7 @@ namespace Architecture.BLL.Services.Implements.ClientProfile
         public async Task<IEnumerable<ProfFamilyInfo>> GetAll(int profileId)
         {
             IEnumerable<ProfFamilyInfo> result;
-              result=await familyInfoRepo.GetAsync(x => x,x => x.ProfileId== profileId);
+              result=await GetAsync(x => x,x => x.ProfileId== profileId);
             return result;
 
         }
