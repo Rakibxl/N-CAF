73faefcb7c7cafcfb97530aa0b0a202acc832242
Rakibxl <Administrator@DESKTOP-H7T44MN>
2021-01-11 19:03:23 +0100

    Added some changes

--- Architecture.Core/Repository/Context/FluentAPIMapping/SectionLinkConfiguration.cs
+++ Architecture.Core/Repository/Context/FluentAPIMapping/SectionLinkConfiguration.cs
@@ -9,9 +9,9 @@ namespace Architecture.Core.Repository.Context.FluentAPIMapping
         public void Configure(EntityTypeBuilder<SectionLink> sectionlink)
         {
             sectionlink.HasKey(bs => bs.SectionLinkId);
-            sectionlink.Property(bs => bs.Title).HasMaxLength(250).IsRequired();
+            sectionlink.Property(bs => bs.Title).HasMaxLength(500).IsRequired();
             sectionlink.Property(bs => bs.ActionLink).HasMaxLength(100).IsRequired();
-            sectionlink.Property(bs => bs.Remarks).HasMaxLength(100).IsRequired(false);
+            sectionlink.Property(bs => bs.Remarks).HasMaxLength(500).IsRequired(false);
             sectionlink.Property(bs => bs.Modified).ValueGeneratedOnAddOrUpdate().HasComputedColumnSql("GetUtcDate()");
             sectionlink.Property(bs => bs.Created).ValueGeneratedOnAddOrUpdate().HasComputedColumnSql("GetUtcDate()");
         }
