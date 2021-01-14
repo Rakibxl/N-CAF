5418351368784760b2ca981b0d48842ebbd1a138
ARIFUL ISLAM <a.islam@stscapital.lt>
2021-01-14 13:53:49 +0600

    working on change password

--- Architecture.Web/ClientApp/src/app/Layout/UserProfile/change-password/change-password.component.ts
+++ Architecture.Web/ClientApp/src/app/Layout/UserProfile/change-password/change-password.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IAuthUser } from '../../../Shared/Entity/Users/auth';
+import { AlertService } from '../../../Shared/Modules/alert/alert.service';
+import { ClientProfileService } from '../../../Shared/Services/ClientProfile/client-profile.service';
+import { CommonService } from '../../../Shared/Services/Common/common.service';
+import { DropdownService } from '../../../Shared/Services/Common/dropdown.service';
+import { AuthService } from '../../../Shared/Services/Users/auth.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +15,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+    changePasswordForm: FormGroup;
+    user: IAuthUser;
+    profileId: any;
+    public gender = [];
+    public maritalStatus = [];
+    public nationality = [];
+    public eyeColor = [];
+    public motiveType = [];
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder, private route: ActivatedRoute, private authService: AuthService,
+        private alertService: AlertService, private router: Router, private commonService: CommonService) {
+        this.authService.currentUser.subscribe(user => this.user = user);
+        this.initForm();
+    }
+
+    async ngOnInit() {
+       
+    }
+
+    initForm() {
+        this.changePasswordForm = this.fb.group({
+            profileId: [0],
+            oldPassword: [null, Validators.required],
+            newPassword: [null, Validators.required],
+            confirmPassword: [null, Validators.required],
+        });
+    }
+
+    backBtnClick() {
+        this.router.navigate(["/client-profile/client-list"]);
+    }
+
+
+    getModel() {
+        let formData = this.changePasswordForm.value;
+        formData.profileId = formData.profileId || 0;
+        return formData;
+    }
+
+    getBoolValue(val) {
+        if (val && (val == 'Yes' || val == '1' || val == 1 || val == true)) {
+            return true;
+        }
+        return false;
+    }
+
+    save() {
+        //let model = this.getModel();
+        //this.clientProfileService.createOrUpdateBasicInfo(model).subscribe(res => {
+        //    if (res && res.data && res.data.profileId) {
+        //        this.alertService.tosterSuccess('Basic Info saved successfully');
+        //        this.basicInfoForm.patchValue({ profileId: res.data.profileId });
+        //        setTimeout(() => {
+        //            this.router.navigate([`/client-profile/client-list`]);
+        //        }, 200);
+        //    }
+        //}, (error: any) => {
+        //    console.log(error);
+        //    this.alertService.tosterDanger(error);
+        //});
+    }
 
 }
--- Architecture.Web/ClientApp/src/app/Layout/UserProfile/user-profile.module.ts
+++ Architecture.Web/ClientApp/src/app/Layout/UserProfile/user-profile.module.ts
@@ -4,13 +4,20 @@ import { CommonModule } from '@angular/common';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { SharedMasterModule } from '../../Shared/Modules/shared-master/shared-master.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ChangePasswordComponent, ViewProfileComponent],
   imports: [
     CommonModule,
-    UserProfileRoutingModule
+      UserProfileRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      NgSelectModule,
+      SharedMasterModule,
   ]
 })
 export class UserProfileModule { }
